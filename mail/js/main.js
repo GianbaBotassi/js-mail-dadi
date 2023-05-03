5b03e8becea7069019d4eda70193aaf9629f0a48
GianbaBotassi <gianba89@gmail.com>
2023-05-03 21:50:58 +0200

    Replace message with inList var, refactoring

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,29 +1,40 @@
 // Declare and assign an array with emails
-const emailList = ["boolean@gmail.com","javascript@gmail.com","ciao@gmail.com"];
+const emailList = ["boolean@gmail.com","javascript@gmail.com","ciao@gmail.com", "luca@gmail.com", "maki@hotmail.it"];
 
 // Assign button and message from DOM
 const myButton = document.getElementById("myButton");
-const message = document.getElementById("message");
+let inputMail;
+let message = document.getElementById("message");
+
+
 
 // Click function to access with email
 myButton.addEventListener("click",
-    function(){
+        function(){
 
         // Assign input value from DOM
-        const inputMail = document.getElementById("inputMail").value;
+        inputMail = document.getElementById("inputMail").value;
 
-        // Put before a negative outcome message
-        message.innerHTML = "You are not allowed to access, try with another email.";
+        // Declare a var if entered email is inside an array
+        let inList = false;
         
         // Then cycle with a condition
         for(let i = 0; i < emailList.length; i++){
         
             // If entered mail is alrdy inside array, print positive message overwriting older
-            if(inputMail == emailList[i]){  
-                message.innerHTML = "You are welcome =)";
-            }else if(inputMail == 0){   //if inputMail is empty, another message
-                message.innerHTML = "Remember to enter an email!!!";
+            if(inputMail == emailList[i]){ 
+                inList = true; 
             }
         }
-    })
 
+        // Message condition if true or false
+        if(inList == true){
+            inList = "You are welcome =)";
+        }else{
+            inList = "You are not allowed to access, try with another email."
+        }
+
+        // Inner html of result to message in DOM
+        message.innerHTML = inList;
+        }
+    )    
