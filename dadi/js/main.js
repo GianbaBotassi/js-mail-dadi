d9a72f16f2a2161ff20639130523f07aaefee960
GianbaBotassi <gianba89@gmail.com>
2023-05-03 19:11:56 +0200

    Added styles and refactoring

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,32 +1,37 @@
-// creo bottone per dare un numero random da 1 a 6
+// Create button for function
 const myButton = document.getElementById("myButton");
 
+// Create click function
 myButton.addEventListener("click",
     function(){
-        // funzione con click da un numero da 1 a 6 all'user
-    const numUser = Math.floor((Math.random() * 6) + 1);
+        // Math random for User
+        const numUser = Math.floor((Math.random() * 6) + 1);
 
-    document.getElementById("numUser").innerHTML = numUser;
-    // da un numero da 1 a 6 al computer
-    const numBot = Math.floor((Math.random() * 6) + 1);
+        // Add to DOM math random user
+        document.getElementById("numUser").innerHTML = numUser;
 
-    document.getElementById("numBot").innerHTML = numBot;
+        // Math random for bot
+        const numBot = Math.floor((Math.random() * 6) + 1);
 
+        // Add to DOM math random Bot
+        document.getElementById("numBot").innerHTML = numBot;
 
-    // condizione per vedere chi ha vinto
-    if(numUser > numBot){
-        result = "you"
-        // Vince User
-    }else if(numUser < numBot){
-        result = "your worst enemy";
-        // Vince Bot
-    }else{
-        result = "draw"
-        // Pareggio
-    }
+        // Assing constant result
+        const result = document.getElementById("result");
 
-    document.getElementById("result").innerHTML = result;
-    ;
+        // Condition to know who win
+        if(numUser > numBot){
+            whoWin = "You"
+            // User win
+        }else if(numUser < numBot){
+            whoWin = "Bot";
+            // Bot win
+        }else{
+            whoWin = "Draw"
+            // Draw
+        }
+        
+        result.innerHTML = whoWin;
     }
     )
 
