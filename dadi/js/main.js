b6c0a49a6c06a9907bb4cdf422a957260e25eb2c
GianbaBotassi <gianba89@gmail.com>
2023-05-03 17:55:39 +0200

    Add some style, need refactoring

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,30 +1,33 @@
 // creo bottone per dare un numero random da 1 a 6
+const myButton = document.getElementById("myButton");
 
-// funzione con click da un numero da 1 a 6 all'user
+myButton.addEventListener("click",
+    function(){
+        // funzione con click da un numero da 1 a 6 all'user
     const numUser = Math.floor((Math.random() * 6) + 1);
 
     document.getElementById("numUser").innerHTML = numUser;
-// da un numero da 1 a 6 al computer
-const numBot = Math.floor((Math.random() * 6) + 1);
+    // da un numero da 1 a 6 al computer
+    const numBot = Math.floor((Math.random() * 6) + 1);
 
-document.getElementById("numBot").innerHTML = numBot;
-
-
-
-// Dichiaro costante chi ha vinto
+    document.getElementById("numBot").innerHTML = numBot;
 
 
-// condizione per vedere chi ha vinto
-if(numUser > numBot){
-    result = "you"
-    // Vince User
-}else if(numUser < numBot){
-    result = "your worst enemy";
-    // Vince Bot
-}else{
-    result = "draw"
-    // Pareggio
-}
+    // condizione per vedere chi ha vinto
+    if(numUser > numBot){
+        result = "you"
+        // Vince User
+    }else if(numUser < numBot){
+        result = "your worst enemy";
+        // Vince Bot
+    }else{
+        result = "draw"
+        // Pareggio
+    }
 
-document.getElementById("result").innerHTML = "and the winner is " + result;
-;
+    document.getElementById("result").innerHTML = result;
+    ;
+    }
+    )
+
+
