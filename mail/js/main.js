3aa035e8b2bac0f06c3d212ed212d9804c89c4d4
GianbaBotassi <gianba89@gmail.com>
2023-05-03 16:54:16 +0200

    Ended mail request

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,33 +1,29 @@
 // Declare and assign an array with emails
-const emailList = ["cighi","luighi","scemo"];
+const emailList = ["boolean@gmail.com","javascript@gmail.com","ciao@gmail.com"];
 
-// Select input and button from DOM
-const inputMail = document.getElementById("inputMail").value;
+// Assign button and message from DOM
 const myButton = document.getElementById("myButton");
+const message = document.getElementById("message");
 
-
-// Click function to send email
+// Click function to access with email
 myButton.addEventListener("click",
     function(){
 
-    // Cycle all elements inside array
-    for(let i = 0; i < emailList.length; i++){
+        // Assign input value from DOM
+        const inputMail = document.getElementById("inputMail").value;
 
-        // Declare a const
-
-        // Condition, if entered mail is alrdy inside array, print positive message
-        if(inputMail == emailList[i]){  
-            alert("You are welcome " + inputMail + "!");
-        }else if(!(inputMail == emailList[i])){
-            alert("icoa")
+        // Put before a negative outcome message
+        message.innerHTML = "You are not allowed to access, try with another email.";
+        
+        // Then cycle with a condition
+        for(let i = 0; i < emailList.length; i++){
+        
+            // If entered mail is alrdy inside array, print positive message overwriting older
+            if(inputMail == emailList[i]){  
+                message.innerHTML = "You are welcome =)";
+            }else if(inputMail == 0){   //if inputMail is empty, another message
+                message.innerHTML = "Remember to enter an email!!!";
+            }
         }
-    }
-
-
     })
-
-
-// Se input è uguale a elemento ciclato della lista
-
-// output positivo o negativo
 
